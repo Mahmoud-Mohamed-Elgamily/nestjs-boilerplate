5012c63423336aaa0111669e69e99ba240e86fae
Gamily <mahmoud.mohamed19@gmail.com>
2023-03-18 01:52:57 +0200

    add env config extract ormconfig into separate file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { ormConfig } from 'config/typeorm.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: '172.17.0.2',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'test',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-    UserModule,
-  ],
+  imports: [TypeOrmModule.forRoot(ormConfig), UserModule],
 
   controllers: [AppController],
   providers: [AppService],
