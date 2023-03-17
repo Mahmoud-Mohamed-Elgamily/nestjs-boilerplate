6045843c81d93a7b371622f4b17881106d53662d
Gamily <mahmoud.mohamed19@gmail.com>
2023-03-17 23:52:03 +0200

    add user repository

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -6,8 +6,8 @@ export class User {
   id: number;
 
   @Column()
-  first_name: string;
+  firstName: string;
 
   @Column()
-  last_name: string;
+  lastName: string;
 }
---
+++ src/user/user.repository.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UserRepository extends Repository<User> {
+  constructor(private dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+
+  async findaaa() {
+    return await this.find();
+  }
+
+  //write other helpful methods here(find, delete, etc...)
+}
