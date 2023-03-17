cf5b437e8d17abd76f0318014c1522d82443d762
Gamily <mahmoud.mohamed19@gmail.com>
2023-03-17 23:31:43 +0200

    add use rentity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryColumn()
+  id: number;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+}
