2953c610b21aca081a0d8f2467ec1f473bda7244
Gamily <mahmoud.mohamed19@gmail.com>
2023-03-20 11:05:41 +0200

    dockerize the app

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.NESTJS_APP_LOCAL_PORT);
+  console.log(`listining on port ${process.env.NESTJS_APP_LOCAL_PORT}`);
 }
 bootstrap();
